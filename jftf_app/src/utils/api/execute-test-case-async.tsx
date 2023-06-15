b04e82cca14a70fa4c2e9073a22ad7eafe5327b4
Decebal <radulescudecebal@outlook.com>
2023-06-16 02:02:07 +0300

    SNAPSHOT-1.0/Experimental - Added async utility function for executing a test case

---
+++ jftf_app/src/utils/api/execute-test-case-async.tsx
@@ -0,0 +1,34 @@
+import {getCSRFToken} from "../auth/get-csrf-token.tsx";
+import axios from "axios";
+import {getTokenFromLocalStorage} from "../auth/get-token-local-storage.tsx";
+import {toast} from "react-toastify";
+
+
+export async function executeTestCase(id: number, runner: string): Promise<string> {
+    const csrfToken: string = getCSRFToken();
+    const authToken: string = getTokenFromLocalStorage();
+
+    const config = {
+        headers: {
+            'X-CSRFToken': csrfToken,
+            'Authorization': `Token ${authToken}`
+        }
+    };
+
+    const url: string = `http://localhost:8000/api/test-case/${id}/execute/`;
+    const requestBody = {
+        runner: runner
+    };
+
+    try {
+        const response = await axios.post(url, requestBody, config);
+
+        // Extract the task ID from the response
+        const taskID: string = response.data.task_id;
+        toast.success(`Test case execution successful: task_id is '${taskID}'`);
+
+        return taskID;
+    } catch (error) {
+        toast.error(`Error executing test case with id '${id}': ${error.response?.data.error || error.message}`);
+    }
+}
