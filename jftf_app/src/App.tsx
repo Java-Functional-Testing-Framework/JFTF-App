8f5b68fff713f2399b3bdf815b16fc05b1882a7c
Decebal <radulescudecebal@outlook.com>
2023-06-16 01:47:25 +0300

    SNAPSHOT-1.0/Experimental - COnfigure react-toastify

--- jftf_app/src/App.tsx
+++ jftf_app/src/App.tsx
@@ -1,4 +1,4 @@
-import { Admin, Resource } from "react-admin";
+import {Admin, Resource} from "react-admin";
 import JFTFRestProvider from './data-providers/jftf-rest-server.ts';
 import {authProvider} from "./auth-providers/jftf-auth-provider.ts";
 import {fetchJsonWithAuthToken} from 'ra-data-django-rest-framework';
@@ -6,14 +6,19 @@ import {JftfAppDefaultLayout} from './layout/jftf-app-default-layout.tsx'
 import {TestCaseAdminList} from "./resource-lists/test-case-list.tsx";
 import {TestCaseMetadataShow} from "./show-components/test-case-metadata-show.tsx";
 import {darkTheme} from "./layout/jftf-app-default-app-bar.tsx";
+import 'react-toastify/dist/ReactToastify.css';
+import {ToastContainer} from "react-toastify";
 
 
-const dataProvider = JFTFRestProvider('http://localhost:8000/api', fetchJsonWithAuthToken, );
+const dataProvider = JFTFRestProvider('http://localhost:8000/api', fetchJsonWithAuthToken,);
 
 const App = () => (
-    <Admin theme={darkTheme} layout={JftfAppDefaultLayout} dataProvider={dataProvider} authProvider={authProvider}>
-        <Resource name="test-case-admin" list={TestCaseAdminList} show={TestCaseMetadataShow}/>
-    </Admin>
+    <>
+        <ToastContainer/>
+        <Admin theme={darkTheme} layout={JftfAppDefaultLayout} dataProvider={dataProvider} authProvider={authProvider}>
+            <Resource name="test-case-admin" list={TestCaseAdminList} show={TestCaseMetadataShow}/>
+        </Admin>
+    </>
 );
 
 export default App;
