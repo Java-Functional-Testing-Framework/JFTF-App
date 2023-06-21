5502af74fb3ccdcf4ab5d127d6c56e931d40fd89
Decebal <radulescudecebal@outlook.com>
2023-06-21 12:12:00 +0300

    SNAPSHOT-1.0/Experimental - Further ordering field refinements

--- jftf_app/src/resource-lists/task-queue-list.tsx
+++ jftf_app/src/resource-lists/task-queue-list.tsx
@@ -7,11 +7,11 @@ export const TaskQueueAdminList = () => (
     <List>
         <Datagrid>
             <TextField source="id"/>
-            <TextField source="task_id" label={"Task Id"}/>
+            <TextField source="task_id" label={"Task Id"} sortable={false}/>
             <TextField source="status" label={"Task status"}/>
             <DateField source="date_created" showTime={true}/>
             <DateField source="date_done" showTime={true}/>
-            <RichTextField source="task_args" label={"Task arguments"}/>
+            <RichTextField source="task_args" label={"Task arguments"} sortable={false}/>
             <TaskResultInformationShowButton label={"Task result information"}/>
         </Datagrid>
     </List>
--- jftf_app/src/resource-lists/test-report-list.tsx
+++ jftf_app/src/resource-lists/test-report-list.tsx
@@ -1,9 +1,9 @@
-import {ReferenceField, Datagrid, DateField, List, TextField, ShowButton} from 'react-admin';
+import {ReferenceField, Datagrid, DateField, List, TextField, ShowButton, SelectField} from 'react-admin';
 
 const TestReportInformationShowButton = (props) => <ShowButton label="Show test report information"/>;
 
 
-export const TestReportAdminList = () => (
+export const TestReportAdminList = (props) => (
     <List>
         <Datagrid>
             <TextField source="id"/>
@@ -11,12 +11,15 @@ export const TestReportAdminList = () => (
             <ReferenceField label="Test case name" source="testId" reference="test-case-admin" sortable={false}>
                 <TextField source="metaData.testName"/>
             </ReferenceField>
-            <TextField source="testReportInformation.executionResult" label={"Execution result"} sortable={false}/>
+            <SelectField source="testReportInformation.executionResult" label={"Execution result"}
+                         choices={[
+                             {id: 'errorState', name: 'FAILED'},
+                             {id: 'successfulState', name: 'SUCCESSFUL'},
+                         ]} sx={{fontWeight: "bold"}}/>
             <DateField source="testReportInformation.startupTimestamp" showTime={true}
-                       label={"Execution startup timestamp"} sortable={false}/>
-            <DateField source="testReportInformation.endTimestamp" showTime={true} label={"Execution end timestamp"}
-                       sortable={false}/>
-            <TextField source="testReportInformation.testDuration" label={"Execution duration"} sortable={false}/>
+                       label={"Execution startup timestamp"}/>
+            <DateField source="testReportInformation.endTimestamp" showTime={true} label={"Execution end timestamp"}/>
+            <TextField source="testReportInformation.testDuration" label={"Execution duration"}/>
             <TestReportInformationShowButton label={"Test report information"}/>
         </Datagrid>
     </List>
